04de8e39349851a562d7ffe15f4375c870d53937
setvect <setvect@naver.com>
2025-08-11 12:45:13 +0900

    eslinit 설정

---
+++ frontend/.eslintrc.cjs
@@ -0,0 +1,38 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2020: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:svelte/recommended',
+    'prettier',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    extraFileExtensions: ['.svelte'],
+    parser: '@typescript-eslint/parser',
+  },
+  plugins: [
+    '@typescript-eslint',
+    'prettier',
+  ],
+  rules: {
+    // Add custom rules here if needed
+    'prettier/prettier': 'error',
+  },
+  overrides: [
+    {
+      files: ['*.svelte'],
+      parser: 'svelte-eslint-parser',
+      parserOptions: {
+        parser: '@typescript-eslint/parser',
+      },
+    },
+  ],
+};
