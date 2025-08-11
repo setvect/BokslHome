2f115c13d08c379ab888abb7a64a45a6e742ff72
setvect <setvect@naver.com>
2025-08-11 20:45:39 +0900

    디자인 설정 초기화

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,14 +1,5 @@
 <script>
   import "../app.css";
-  import { theme } from "$lib/stores/theme";
-  import { onMount } from "svelte";
-
-  // SvelteKit에서 자동으로 전달하는 모든 props를 무시
-  $: void $$restProps;
-
-  onMount(() => {
-    theme.init();
-  });
 </script>
 
 <slot />
